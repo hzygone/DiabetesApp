83be3cf3493242f0c48f1d38b04c05f9a9e487f3
James Edwards <hzy_@live.com>
2016-08-16 20:10:43 -0500

    alarm

---
+++ app/src/main/java/home/diabetesapp/AlarmReceiver.java
@@ -0,0 +1,21 @@
+package home.diabetesapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // For our recurring task, we'll just display a message
+        Toast.makeText(context, "You have a regimen activity to complete.", Toast.LENGTH_SHORT).show();
+    }
+}
